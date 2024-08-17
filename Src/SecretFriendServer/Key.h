806fa50d6393cfa54119bef3c3fc550b3baf7817
Eunji <hyni03@naver.com>
2024-08-18 01:28:50 +0900

    Feat: add key class #EJ

--- Src/SecretFriendServer/Key.h
+++ Src/SecretFriendServer/Key.h
@@ -1,4 +1,23 @@
 #pragma once
+#ifndef KEY_H
+#define KEY_H
 
-#include <mutex>
+#include <vector>
+#include <mutex> 
+#include "Session.h"
 
+class KeyManager
+{
+public:
+    // 수신된 공개 키를 세션에 저장
+    bool ReceivePublicKey(Session* requestorSession, const std::array<BYTE, RSA_KEY_SIZE>& data);
+    bool ReceiveSymmetricKey(Session* requestorSession, const std::array<BYTE, AES_KEY_SIZE>& data);
+    bool ReceiveRoomSymmetricKey(Session* requestorSession, const std::array<BYTE, RSA_KEY_SIZE>& data);
+    bool SendGuestPublicKey(Session* requestorSession);
+    bool SendRoomSymmetricKey(Session* requestorSession, const std::array<BYTE, RSA_KEY_SIZE>& data);
+
+private:
+    std::mutex mtx; 
+};
+
+#endif
